909728e312132ee52550fe0cc727aba59bc80d8a
yogmicroservices <yogmangela@hotmail.co.uk>
2023-03-19 14:59:46 +0000

    Added Docker image build step 0.0.3

--- hello-world/src/app.py
+++ hello-world/src/app.py
@@ -164,7 +164,7 @@ api.add_resource(Divide, "/division")
 
 @app.route('/')
 def hello_world():
-    return "Hello Universe How are you...0.0.3"
+    return "Hello Universe How are you-0.0.3"
 
 
 if __name__=="__main__":
