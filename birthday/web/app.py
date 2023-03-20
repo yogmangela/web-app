21c9be2dda44f3957ac57c58ad0cda30793176d1
yogmicroservices <yogmangela@hotmail.co.uk>
2023-03-20 11:30:21 +0000

    Adding Birthday app

--- birthday/web/app.py
+++ birthday/web/app.py
@@ -5,38 +5,24 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 api = Api(app)
 
-class Hello(Resource):
-    def post():
-        
-        postedData = request.get_json()
-        year = postedData[int(input('enter the year you were born - yy:'))]
-        month = postedData[int(input('enter the month you were born - mm: '))]
-        day = postedData[int(input('enter the day you were born - dd: '))]
+def get_user_birthday():
+    year = int(input('Please enter the year of your birth [YY] '))
+    month = int(input('Please enter the month of your birth [MM] '))
+    day = int(input('Please enter the day of your birth [DD] '))
 
-        birthday = datetime(2000+year,month,day)
-        return birthday
-        retMap = {
-            'Message': birthday,
-            'Status Code': 200
-        }
-        return jsonify(retMap)
+    birthday = datetime(2000+year,month,day)
+    return birthday
 
-
-# def calculate_dates(original_date, now):
-#     d1 = datetime(now.year, original_date.month, original_date.day)
-#     d2 = datetime(now.year+1, original_date.month, original_date.day)
+def calculate_dates(original_date, now):
+    x = datetime(now.year, original_date.month, original_date.day)
+    y = datetime(now.year+1, original_date.month, original_date.day)
     
-#     return ((d1 if d1 > now else d2) - now).days
+    return ((x if x > now else y) - now).days
 
-# bd = get_birthday()
-# now = datetime.now()
-# c = calculate_dates(bd, now)
-
-# print(c)
-
-
-api.add_resource(Hello, "/hello")
-
+birthday = get_user_birthday()
+present = datetime.now()
+days_to_birthday = calculate_dates(birthday, present)
+print(f" ${days_to_birthday} days let to your birthday")
 
 @app.route('/')
 def hello_world():
@@ -44,21 +30,3 @@ def hello_world():
 
 if __name__=="__main__":
     app.run(host='0.0.0.0')
-
-
-# from datetime import date
-
-# year = int(input('Year:'))
-# month = int(input('Month:'))
-# day = int(input('Day:'))
-
-# birthday = date(year, month, day)
-# today = date.today()
-# next_birthday = birthday.replace(year=today.year)
-
-# if next_birthday < today:
-#     # birthday for this year has already passed
-#     next_birthday = next_birthday.replace(year=next_birthday.year + 1)
-
-# print('You are', int((today - birthday).days / 365), 'years old')
-# print('Your next birthday is in', (next_birthday - today).days, 'days')
